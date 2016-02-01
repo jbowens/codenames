29b6e87626ea41d40baf64a177fd1ffbc9009eb6
Jackson Owens <jackson@chain.com>
2016-01-31 17:58:27 -0800

    join game, retrieve game

--- game.go
+++ game.go
@@ -2,6 +2,7 @@ package codenames
 
 import (
 	"encoding/json"
+	"errors"
 	"math/rand"
 
 	"github.com/satori/go.uuid"
@@ -43,12 +44,47 @@ func (t Team) Repeat(n int) []Team {
 }
 
 type Game struct {
-	ID           string   `json:"id"`
-	Name         string   `json:"name"`
-	StartingTeam Team     `json:"starting_team"`
-	Round        int      `json:"round"`
-	Words        []string `json:"words"`
-	Layout       []Team   `json:"layout"`
+	ID           string             `json:"id"`
+	Name         string             `json:"name"`
+	StartingTeam Team               `json:"starting_team"`
+	Round        int                `json:"round"`
+	Words        []string           `json:"words"`
+	Layout       []Team             `json:"layout"`
+	Players      map[string]*Player `json:"players"`
+	Codemaster   map[Team]*Player   `json:"codemaster"`
+}
+
+func (g *Game) AddPlayer(p *Player, codemaster bool) error {
+	if g.Round > 0 {
+		return errors.New("That game has already started.")
+	}
+
+	if p.ID == "" {
+		p.ID = uuid.NewV4().String()
+	}
+
+	g.Players[p.ID] = p
+
+	if codemaster && p.Team != Neutral {
+		g.Codemaster[p.Team] = p
+	}
+	return nil
+}
+
+func (g *Game) CurrentTeam() Team {
+	if g.Round%2 == 0 {
+		return g.StartingTeam
+	}
+	if g.StartingTeam == Red {
+		return Blue
+	}
+	return Red
+}
+
+type Player struct {
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name"`
+	Team Team   `json:"team"`
 }
 
 func newGame(name string, words []string) *Game {
@@ -58,6 +94,8 @@ func newGame(name string, words []string) *Game {
 		StartingTeam: Team(rand.Intn(2)) + Red,
 		Words:        make([]string, 0, wordsPerGame),
 		Layout:       make([]Team, 0, wordsPerGame),
+		Codemaster:   make(map[Team]*Player),
+		Players:      make(map[string]*Player),
 	}
 
 	// Pick 25 random words.
--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package codenames
 import (
 	"encoding/json"
 	"net/http"
+	"path"
 	"sync"
 
 	"github.com/jbowens/dictionary"
@@ -38,6 +39,53 @@ func (s *Server) handleNewGame(rw http.ResponseWriter, req *http.Request) {
 	writeJSON(rw, g)
 }
 
+// POST /join
+func (s *Server) handleJoinGame(rw http.ResponseWriter, req *http.Request) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	var request struct {
+		GameID     string  `json:"game_id"`
+		Codemaster bool    `json:"codemaster"`
+		Player     *Player `json:"player"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	if err := decoder.Decode(&request); err != nil {
+		http.Error(rw, "Error decoding", 400)
+		return
+	}
+
+	g, ok := s.games[request.GameID]
+	if !ok {
+		http.Error(rw, "No such game", 404)
+		return
+	}
+
+	err := g.AddPlayer(request.Player, request.Codemaster)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+
+	writeJSON(rw, g)
+}
+
+// GET /game/<id>
+func (s *Server) handleRetrieveGame(rw http.ResponseWriter, req *http.Request) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	gameID := path.Base(req.URL.Path)
+	g, ok := s.games[gameID]
+	if !ok {
+		http.Error(rw, "No such game", 404)
+		return
+	}
+
+	writeJSON(rw, g)
+}
+
 func (s *Server) Start() error {
 	d, err := dictionary.Default()
 	if err != nil {
@@ -49,6 +97,8 @@ func (s *Server) Start() error {
 
 	s.mux = http.NewServeMux()
 	s.mux.HandleFunc("/new", s.handleNewGame)
+	s.mux.HandleFunc("/join", s.handleJoinGame)
+	s.mux.HandleFunc("/game/", s.handleRetrieveGame)
 
 	s.games = make(map[string]*Game)
 	s.words = d.Words()
