d8abae6432caa8e7054aebba0f3d803bcf13d698
Alexander Hans <mail@alexanderhans.me>
2019-10-04 20:24:15 +0200

    Multiple language support (#20)
    * added support for multiple languages
    
    * changed name to 'German'

--- frontend/ui/lobby.tsx
+++ frontend/ui/lobby.tsx
@@ -1,77 +1,76 @@
 import * as React from 'react';
-import { OriginalWords, WordsPicker } from '~/ui/words_picker';
+import WordsPicker from '~/ui/words_picker';
+import OriginalWords from '~/words.json';
 
 // TODO: remove jquery dependency
 // https://stackoverflow.com/questions/47968529/how-do-i-use-jquery-and-jquery-ui-with-parcel-bundler
 var jquery = require('jquery');
 window.$ = window.jQuery = jquery;
 
-export class Lobby extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newGameName: this.props.defaultGameID,
-      selectedGame: null,
-      words: OriginalWords,
-    };
-  }
+export const Lobby = ({ defaultGameID }) => {
+  const [newGameName, setNewGameName] = React.useState(defaultGameID);
+  const [selectedLanguage, setSelectedLanguage] = React.useState('English');
+  const [words, setWords] = React.useState(OriginalWords);
 
-  public newGameTextChange(e) {
-    this.setState({ newGameName: e.target.value });
-  }
-
-  public handleNewGame(e) {
+  function handleNewGame(e) {
     e.preventDefault();
-    if (!this.state.newGameName) {
+    if (!newGameName) {
       return;
     }
 
-    const gameID = this.state.newGameName;
     $.post(
       '/next-game',
       JSON.stringify({
-        game_id: gameID,
-        word_set: this.state.words.split(', '),
+        game_id: newGameName,
+        word_set: words[selectedLanguage].split(', '),
       }),
       g => {
-        const newURL = (document.location.pathname = '/' + gameID);
+        const newURL = (document.location.pathname = '/' + newGameName);
         window.location = newURL;
       }
     );
   }
 
-  public onChangedWords(words) {
-    this.setState({ words: words });
-  }
+  return (
+    <div id="lobby">
+      <div id="available-games">
+        <form id="new-game">
+          <p className="intro">
+            Play Codenames online across multiple devices on a shared board. To
+            create a new game or join an existing game, enter a game identifier
+            and click 'GO'.
+          </p>
+          <input
+            type="text"
+            id="game-name"
+            autoFocus
+            onChange={e => {
+              setNewGameName(e.target.value);
+            }}
+            value={newGameName}
+          />
+          <button disabled={!newGameName.length} onClick={handleNewGame}>
+            Go
+          </button>
 
-  public render() {
-    return (
-      <div id="lobby">
-        <div id="available-games">
-          <form id="new-game">
-            <p className="intro">
-              Play Codenames online across multiple devices on a shared board.
-              To create a new game or join an existing game, enter a game
-              identifier and click 'GO'.
-            </p>
-            <input
-              type="text"
-              id="game-name"
-              autoFocus
-              onChange={this.newGameTextChange.bind(this)}
-              value={this.state.newGameName}
-            />
-            <button onClick={this.handleNewGame.bind(this)}>Go</button>
-
-            <div id="new-game-options">
+          <div id="new-game-options">
+            {Object.keys(OriginalWords).map(_language => (
               <WordsPicker
-                words={this.state.words}
-                onChange={this.onChangedWords.bind(this)}
+                key={_language}
+                words={words[_language]}
+                onWordChange={e => {
+                  setWords({ ...words, [_language]: e.target.value });
+                }}
+                language={_language}
+                selectedLanguage={selectedLanguage}
+                onSelectedLanguageChange={() => {
+                  setSelectedLanguage(_language);
+                }}
               />
-            </div>
-          </form>
-        </div>
+            ))}
+          </div>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
