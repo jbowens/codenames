03bf33a1bfe2ef9acd75301ea4ba1ce23669b1f0
Jackson Owens <jackson_owens@alumni.brown.edu>
2020-04-12 18:14:52 -0400

    bump frontend v query param

--- frontend.go
+++ frontend.go
@@ -12,7 +12,7 @@ const tpl = `
 <html>
     <head>
         <title>Codenames - Play Online</title>
-        <script src="/static/app.js?v=0.01" type="text/javascript"></script>
+        <script src="/static/app.js?v=0.02" type="text/javascript"></script>
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="/static/game.css" />
         <link rel="stylesheet" type="text/css" href="/static/lobby.css" />
