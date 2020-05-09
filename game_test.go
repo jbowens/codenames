6d4157067e35427e73eb089d0952cb72c067aad6
Jackson Owens <jackson_owens@alumni.brown.edu>
2020-05-09 11:42:55 -0400

    server: add TestGameShuffle

--- game_test.go
+++ game_test.go
@@ -7,6 +7,16 @@ import (
 	"github.com/jbowens/dictionary"
 )
 
+var testWords []string
+
+func init() {
+	d, err := dictionary.Load("assets/original.txt")
+	if err != nil {
+		panic(err)
+	}
+	testWords = d.Words()
+}
+
 func BenchmarkGameMarshal(b *testing.B) {
 	b.StopTimer()
 	d, err := dictionary.Load("assets/original.txt")
@@ -25,5 +35,24 @@ func BenchmarkGameMarshal(b *testing.B) {
 		if err != nil {
 			b.Fatal(err)
 		}
+	}
+}
+
+func TestGameShuffle(t *testing.T) {
+	gamesWithoutRepeats := len(testWords)/25 - 1
+
+	initialState := randomState(testWords)
+	currState := initialState
+
+	m := map[string]int{}
+	for i := 0; i < gamesWithoutRepeats; i++ {
+		g := newGame("foo", currState, GameOptions{})
+		for _, w := range g.Words {
+			if prevI, ok := m[w]; ok {
+				t.Errorf("Word %q appeared twice, once in game %d and once in game %d.", w, prevI, i)
+			}
+			m[w] = i
+		}
+		currState = nextGameState(currState)
 	}
 }
