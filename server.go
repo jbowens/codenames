f45b9851ce758381d2df396cd9e3d30dfdc60a65
Tom Hebb <tommyhebb@gmail.com>
2020-12-06 12:48:32 -0800

    server: remove now-unused ok return value from getGame(), kill getGameLocked() (#141)
    * server: remove ok return value from getGame()
    
    Since commit 9c8891f4e4bf ("remove unused state ID, unlock s.mu before
    writing game"), there is no code path where a false `ok` parameter will
    be returned. As such, remove the parameter entirely as well as all the
    error handling that existed for it.
    
    * server: remove getGameLocked() function
    
    The only remaining client of this can just use `getGame()`, since it
    takes the lock for no other statements. Make that change, and merge
    `getGameLocked()` into `getGame()`.

--- server.go
+++ server.go
@@ -122,20 +122,17 @@ func (gh *GameHandle) MarshalJSON() ([]byte, error) {
 	return gh.marshaled, err
 }
 
-func (s *Server) getGame(gameID string) (*GameHandle, bool) {
+func (s *Server) getGame(gameID string) *GameHandle {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	return s.getGameLocked(gameID)
-}
 
-func (s *Server) getGameLocked(gameID string) (*GameHandle, bool) {
 	gh, ok := s.games[gameID]
 	if ok {
-		return gh, ok
+		return gh
 	}
 	gh = newHandle(newGame(gameID, randomState(s.defaultWords), GameOptions{}), s.Store)
 	s.games[gameID] = gh
-	return gh, true
+	return gh
 }
 
 // POST /game-state
@@ -150,16 +147,7 @@ func (s *Server) handleGameState(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	s.mu.Lock()
-	gh, ok := s.getGameLocked(body.GameID)
-	if !ok {
-		gh = newHandle(newGame(body.GameID, randomState(s.defaultWords), GameOptions{}), s.Store)
-		s.games[body.GameID] = gh
-		s.mu.Unlock()
-		writeGame(rw, gh)
-		return
-	}
-	s.mu.Unlock()
+	gh := s.getGame(body.GameID)
 
 	updated, replaced := gh.gameStateChanged(body.StateID)
 
@@ -171,11 +159,7 @@ func (s *Server) handleGameState(rw http.ResponseWriter, req *http.Request) {
 	case <-updated:
 		writeGame(rw, gh)
 	case <-replaced:
-		gh, ok = s.getGame(body.GameID)
-		if !ok {
-			http.Error(rw, "Game removed", 400)
-			return
-		}
+		gh = s.getGame(body.GameID)
 		writeGame(rw, gh)
 	}
 }
@@ -193,11 +177,7 @@ func (s *Server) handleGuess(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	gh, ok := s.getGame(request.GameID)
-	if !ok {
-		http.Error(rw, "No such game", 404)
-		return
-	}
+	gh := s.getGame(request.GameID)
 
 	var err error
 	gh.update(func(g *Game) bool {
@@ -224,11 +204,7 @@ func (s *Server) handleEndTurn(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	gh, ok := s.getGame(request.GameID)
-	if !ok {
-		http.Error(rw, "No such game", 404)
-		return
-	}
+	gh := s.getGame(request.GameID)
 
 	gh.update(func(g *Game) bool {
 		return g.NextTurn(request.CurrentRound)
